ab934178f926f53d0918e400b365ebbec0ada8f1
vziks <vziks@live.ru>
2020-09-11 11:58:22 +0300

    (update) Amount numbers in String+

--- src/amountnumbers/Amount.java
+++ src/amountnumbers/Amount.java
@@ -1,29 +1,43 @@
 package amountnumbers;
 
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 /**
  * Class Amount
  *
  *
- * Amount sum of numbers in String
+ * Amount sum of numbers in S
  * Project logictask
  *
  * @author Anton Prokhorov <vziks@live.ru>
  */
 public class Amount {
 
+    static <T> Predicate<T> not(Predicate<T> p) { return o -> !p.test(o); }
+    static Predicate<String> blank = String::isEmpty;
 
     public static void main(String[] args) {
-        System.out.println(amount("111ascas3asc3dc31"));
-        System.out.println(amount("22323m231m1"));
+        System.out.println(amountBlank("111ascas3asc3dc31"));
+        System.out.println(amountNot("111ascas3asc3dc31"));
+        System.out.println(amountBlank("22323m231m1"));
+        System.out.println(amountNot("22323m231m1"));
 
     }
 
-    static int amount(String str) {
+    static int amountBlank(String str) {
+        Predicate<String> blank = String::isEmpty;
         return Pattern.compile("\\D+")
                 .splitAsStream(str)
-                .filter(s -> !s.isEmpty())
+                .filter(Amount.blank.negate())
+                .mapToInt(Integer::parseInt).sum();
+    }
+
+    static int amountNot(String str) {
+        Predicate<String> blank = String::isEmpty;
+        return Pattern.compile("\\D+")
+                .splitAsStream(str)
+                .filter(not(String::isEmpty))
                 .mapToInt(Integer::parseInt).sum();
     }
 }
