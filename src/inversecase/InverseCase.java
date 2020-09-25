a991d4fca037fbe555d5e0313c1e6377e8c387e0
vziks <vziks@live.ru>
2020-09-25 15:40:08 +0300

    (update) InverseCase+

--- src/inversecase/InverseCase.java
+++ src/inversecase/InverseCase.java
@@ -18,16 +18,17 @@ public class InverseCase {
     private static String inverseWithArray(String srt) {
         char[] chars = srt.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            chars[i] = Character.isLowerCase(chars[i]) ? Character.toUpperCase(chars[i])
+            chars[i] = (
+                    Character.isLetter(chars[i]) & Character.isLowerCase(chars[i]))
+                    ? Character.toUpperCase(chars[i])
                     : Character.toLowerCase(chars[i]);
         }
         return new String(chars);
     }
 
     private static String inverseWithStream(String str) {
-        return str.chars().map(c -> Character.isLowerCase(c) ? Character.toUpperCase(c) : Character.toLowerCase(c))
+        return str.chars().map(c -> (Character.isLetter(c) & Character.isLowerCase(c)) ? Character.toUpperCase(c) : Character.toLowerCase(c))
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
     }
-
 
 }
