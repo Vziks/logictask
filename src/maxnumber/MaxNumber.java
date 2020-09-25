29dac6ab9b4fe0463ded36eddb0d3911c09e2afd
vziks <vziks@live.ru>
2020-09-25 17:29:36 +0300

    (update) Maximum number of combinations of numbers

---
+++ src/maxnumber/MaxNumber.java
@@ -0,0 +1,64 @@
+package maxnumber;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Class MaxNumber
+ * Project logictask
+ *
+ * @author Anton Prokhorov <vziks@live.ru>
+ */
+public class MaxNumber {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        List<Integer> list = new ArrayList<Integer>() {{
+            add(2);
+            add(6);
+            add(7);
+            add(0);
+            add(0);
+            add(0);
+            add(0);
+            add(4);
+            add(8);
+            add(8);
+            add(5);
+            add(5);
+            add(3);
+            add(8);
+            add(3);
+            add(3);
+            add(9);
+            add(1);
+        }};
+
+
+        for (int i = 0; i < list.size(); i++) {
+            int max = 0;
+            int s = 0;
+            Integer index;
+            index = null;
+            for (Integer y :
+                    list) {
+                if (y >= 0 && max <= y && !map.containsKey(s)) {
+                    index = s;
+                    max = y;
+                }
+                s++;
+            }
+            map.put(index, max);
+        }
+
+
+        String brutForceString;
+        brutForceString = map.values().stream().map(Object::toString).collect(Collectors.joining(""));
+
+        System.out.println(brutForceString);
+
+    }
+}
