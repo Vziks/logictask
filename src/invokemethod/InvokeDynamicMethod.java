60588a47a688881363995b495d9da4a1fe156851
vziks <vziks@live.ru>
2020-09-28 14:04:50 +0300

    (update) Invoke dynamic method

---
+++ src/invokemethod/InvokeDynamicMethod.java
@@ -0,0 +1,40 @@
+package invokemethod;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Class InvokeDynamicMethod
+ * Project logictask
+ *˚
+ * @author Anton Prokhorov <vziks@live.ru>
+ */
+public class InvokeDynamicMethod {
+
+    public static Object objectMethod() {
+        return new Object() {
+            int sum(int a, int b) {
+                return a + b;
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        try {
+            Object obj = objectMethod();
+            Class[] partypes = new Class[2];
+            partypes[0] = Integer.TYPE;
+            partypes[1] = Integer.TYPE;
+            Method o = obj.getClass().getDeclaredMethod("sum", partypes);
+
+            Object arglist[] = new Object[2];
+            arglist[0] = 37;
+            arglist[1] = 47;
+
+            System.out.println(o.invoke(obj, arglist));
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
