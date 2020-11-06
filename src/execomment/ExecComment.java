5abce2c7753eaf39c245bd27432a7d81da2e7258
vziks <vziks@live.ru>
2020-11-06 15:00:51 +0300

    (update) Executable comment

---
+++ src/execomment/ExecComment.java
@@ -0,0 +1,15 @@
+package execomment;
+
+/**
+ * Class ExecComment
+ * Project logictask
+ *
+ * @author Anton Prokhorov <vziks@live.ru>
+ */
+public class ExecComment {
+
+    public static void main(String[] args) {
+        // Java compiler interprets the Unicode character \u000d
+        // \u000d System.out.println("executable comment");
+    }
+}
