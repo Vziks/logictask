35d30f1c0aff2956fbccdbc0b012fa4e736c19e2
vziks <vziks@live.ru>
2021-03-18 12:35:38 +0300

    (update) NullReference

---
+++ src/nullreference/NullReference.java
@@ -0,0 +1,22 @@
+package nullreference;
+
+/**
+ * Class NullReference
+ * Project logictask
+ *
+ * @author Anton Prokhorov <vziks@live.ru>
+ */
+public class NullReference {
+
+    public static void main(String[] args) throws Throwable {
+        NullReference nullReference1 = null;
+        NullReference nullReference2 = null;
+
+//        NullPointerException
+//        nullReference2 = nullReference1.clone();
+//        NullPointerException
+//        nullReference1.class.getSimpleName();
+
+
+    }
+}
